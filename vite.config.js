214ab0aad3304e22e051a718dda62574746157df
Lena Gadomska <lenaijulka@gmail.com>
2026-04-22 22:14:03 +0200

    dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PromoSearch/' // MUST match your repo name
+  base: '/PromoSearch/' // MUST match repo name exactly
 })
